7da5cd0ff5b823c73d22972ca065ba6e9b0d058b
debboutr <debboutr@gmail.com>
2023-11-05 02:57:11 -0800

    make venv here for simplicity

--- chadrc.lua
+++ chadrc.lua
@@ -4,7 +4,7 @@ M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
 
 M.ui = {
-  transparency = false,
+  transparency = true,
   hl_override = {
     LineNr = {
       fg = "teal",
@@ -13,8 +13,10 @@ M.ui = {
 }
 
 vim.wo.relativenumber = true
+vim.o.textwidth = 72
 
-vim.g.python3_host_prog = '/usr/bin/python3'
+vim.g.python3_host_prog = '/home/rick/.config/nvim/lua/custom/venv/bin/python'
 vim.g.loaded_python3_provider = nil
 
 return M
+
