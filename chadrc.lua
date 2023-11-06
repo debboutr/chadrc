7f177667c49b66d9804055152c27f9ee963b9493
debboutr <debboutr@gmail.com>
2023-11-05 22:27:02 -0800

    change statusline

--- chadrc.lua
+++ chadrc.lua
@@ -2,8 +2,15 @@
 local M = {}
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
-
 M.ui = {
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "round",
+    overriden_modules = nil,
+  },
   transparency = true,
   hl_override = {
     LineNr = {
